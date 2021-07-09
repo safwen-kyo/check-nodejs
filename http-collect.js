0ce60d101df58f3aa6de743931df334cae0d749f
safwen <safwanetoumia@gmail.com>
2021-07-10 00:36:01 +0200

    first commit

---
+++ http-collect.js
@@ -0,0 +1,13 @@
+
+var http = require('http');
+var fullString = '';
+http.get(process.argv[2], function callback(response) {
+
+    response.on('data', function(data) {
+        fullString += data.toString();
+    });
+    response.on('end', function() {
+        console.log(fullString.length);
+        console.log(fullString);
+    });
+});
